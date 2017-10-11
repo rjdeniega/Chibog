38feaa78bcdd5a3b425f9965da025093194dfd7e
Ira Macazo <ira_macazo@dlsu.edu.ph>
2017-10-11 23:27:19 +0800

    Added the google places dependency

--- app/src/main/java/edu/dlsu/mobidev/chibog/MainActivity.java
+++ app/src/main/java/edu/dlsu/mobidev/chibog/MainActivity.java
@@ -3,11 +3,18 @@ package edu.dlsu.mobidev.chibog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.places.GeoDataApi;
+import com.google.android.gms.location.places.Places;
+
 public class MainActivity extends AppCompatActivity {
+
+    protected GeoDataApi mGeoDataClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
