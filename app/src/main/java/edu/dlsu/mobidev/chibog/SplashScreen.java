69ff87bc9a827a3f95446d6031fea1e622ccd15c
rjdeniega <ronald_deniega@dlsu.edu.ph>
2017-10-13 07:05:20 +0800

    Added Test Splash Screen
    splash screen on start up as replacement for login

---
+++ app/src/main/java/edu/dlsu/mobidev/chibog/SplashScreen.java
@@ -0,0 +1,33 @@
+package edu.dlsu.mobidev.chibog;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    //thread sleeps at 2s
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+
+                    //go back to main activity
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        //start the thread
+        myThread.start();
+    }
+}
