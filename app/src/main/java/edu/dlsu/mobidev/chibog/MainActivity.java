3245a138b3ee0f80665b5f4d44520da34e99da4c
iramacazo <ira_macazo@dlsu.edu.ph>
2017-10-12 18:16:57 +0800

    Tested out getting places using Places API

--- app/src/main/java/edu/dlsu/mobidev/chibog/MainActivity.java
+++ app/src/main/java/edu/dlsu/mobidev/chibog/MainActivity.java
@@ -1,20 +1,99 @@
 package edu.dlsu.mobidev.chibog;
 
+import android.*;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.location.places.GeoDataApi;
-import com.google.android.gms.location.places.Places;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    protected GeoDataApi mGeoDataClient;
+    TextView place_name_text, place_address_text;
+    WebView attribution_text;
+    Button get_place;
+    private final static int MY_PERMISSION_FINE_LOCATION = 101;
+    private final static int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestPermission();
 
+        place_name_text = (TextView) findViewById(R.id.place_name);
+        place_address_text = (TextView) findViewById(R.id.place_address);
+        attribution_text = (WebView) findViewById(R.id.web_attribution);
+        get_place = (Button) findViewById(R.id.get_place);
+        get_place.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try{
+                    Intent intent = builder.build(MainActivity.this);
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void requestPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_FINE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case MY_PERMISSION_FINE_LOCATION:
+                if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "This app requires location services", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PLACE_PICKER_REQUEST){
+            if(resultCode == RESULT_OK){
+                Place place = PlacePicker.getPlace(MainActivity.this, data);
+                place_name_text.setText(place.getName());
+                place_address_text.setText(place.getAddress());
+                if (place.getAttributions() == null){
+                    attribution_text.loadData("no attribution", "text/html; charset=utf-8", "UTF-8");
+                }else{
+                    attribution_text.loadData(place.getAttributions().toString(), "text/html; charset=utf-8", "UTF-8");
+                }
+            }
+        }
     }
 }
