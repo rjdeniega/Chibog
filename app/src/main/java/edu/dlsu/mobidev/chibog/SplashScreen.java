08678d8e26117ea56fa436497f471232ca61069b
rjdeniega <ronald_deniega@dlsu.edu.ph>
2017-10-15 23:10:39 +0800

    Changed temporary splash + action bar
    di pala nagpush LOL

--- app/src/main/java/edu/dlsu/mobidev/chibog/SplashScreen.java
+++ app/src/main/java/edu/dlsu/mobidev/chibog/SplashScreen.java
@@ -1,22 +1,31 @@
 package edu.dlsu.mobidev.chibog;
 
 import android.content.Intent;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
-
+    public ImageView splashImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash_screen);
+
+//        splashImage = (ImageView) findViewById(R.id.splashimage);
+//        RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(getResources(), src);
+//        dr.setCornerRadius(30);
+//        splashImage.setImageDrawable(dr);
 
         Thread myThread = new Thread(){
             @Override
             public void run() {
                 try {
                     //thread sleeps at 2s
-                    sleep(1500);
+                    sleep(2000);
                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                     startActivity(intent);
 
